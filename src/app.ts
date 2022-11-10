24176b0ef40ceda81f9f21a0d678eec7df237e1a
Sean <sean@sscottdev.com>
2022-11-09 23:29:11 -0500

    timer & fps

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import RenderEngine from "./RenderEngine";
 import Snake from "./Snake";
 import Apple from "./Apple";
 import Controller from "./Controller";
+import apple from "./Apple";
 
 class Game {
     private _gameView: GameView;
@@ -13,6 +14,9 @@ class Game {
     private _snake: Snake;
     private _apple: Apple;
     private _p1: Controller;
+    private _p2: Controller;
+    private _fps: number = 10;
+    private _timer: number = 0;
 
     constructor() {
         this._gameView = new GameView(10, 20, 20);
@@ -23,11 +27,12 @@ class Game {
         this._snake = new Snake(3, "#FFA500", "#00FF00", this._gameView.gridSquareSize, this._gameView.gridSquareSize, this._gameView.gridSquareSize * 2, this._gameView.gridSquareSize * 2);
         this._apple = new Apple(this._gameView.gridSquareSize * 5, this._gameView.gridSquareSize * 5, this._gameView.gridSquareSize, this._gameView.gridSquareSize, "#FF0000");
         this._p1 = new Controller('w', 's', 'a', 'd', this._snake, this._gameView.gridSquareSize);
+        this._p2 = new Controller('ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight', this._apple, this._gameView.gridSquareSize);
     }
 
     startGame = () => {
         this.gameLoop();
-        this._gameLoop = setInterval(this.gameLoop, 1000);
+        this._gameLoop = setInterval(this.gameLoop, 1000 / this._fps);
     }
 
     stopGame = () => {
@@ -37,9 +42,15 @@ class Game {
         this._gameView.context.clearRect(0, 0, this._gameView.canvas.width, this._gameView.canvas.height);
 
         this._p1.update();
+        this._p2.update();
         this._renderEngine.addRenderable(this._snake);
         this._renderEngine.addRenderable(this._apple);
         this._renderEngine.render();
+        this._timer += 1;
+        if (this._timer % 10 == 0) {
+            this._fps += 1;
+            this._gameLoop = setInterval(this.gameLoop, 1000 / this._fps);
+        }
         console.log("game loop");
     }
 }
