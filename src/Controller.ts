24b2b55f7973e9beb22fb69c2de83f0940dc8610
Sean <sean@sscottdev.com>
2022-11-09 23:11:46 -0500

    controller

---
+++ src/Controller.ts
@@ -0,0 +1,36 @@
+class Controller {
+    private _up: string;
+    private _down: string;
+    private _left: string;
+    private _right: string;
+    private _renderable: Renderable;
+    private _direction: string = "Alt";
+    private _tileSize: number;
+
+    constructor(up: string, down: string, left: string, right: string, renderable: Renderable, tileSize: number) {
+        this._up = up;
+        this._down = down;
+        this._left = left;
+        this._right = right;
+        this._renderable = renderable;
+        this._tileSize = tileSize;
+        window.addEventListener('keydown', (event) => {
+            switch (event.key) {
+                case this._up: this._direction = 'up'; break;
+                case this._down: this._direction = 'down'; break;
+                case this._left: this._direction = 'left'; break;
+                case this._right: this._direction = 'right'; break;
+            }
+        });
+    }
+    update() {
+        switch (this._direction) {
+            case 'up': this._renderable.y -= this._tileSize; break;
+            case 'down': this._renderable.y += this._tileSize; break;
+            case 'left': this._renderable.x -= this._tileSize; break;
+            case 'right': this._renderable.x += this._tileSize; break;
+        }
+    }
+}
+
+export default Controller;
--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import GameView from "./GameView";
 import RenderEngine from "./RenderEngine";
 import Snake from "./Snake";
 import Apple from "./Apple";
+import Controller from "./Controller";
 
 class Game {
     private _gameView: GameView;
@@ -9,6 +10,9 @@ class Game {
     private _startButton: HTMLButtonElement = document.getElementById("startButton") as HTMLButtonElement;
     private _stopButton: HTMLButtonElement = document.getElementById("stopButton") as HTMLButtonElement;
     private _renderEngine: RenderEngine;
+    private _snake: Snake;
+    private _apple: Apple;
+    private _p1: Controller;
 
     constructor() {
         this._gameView = new GameView(10, 20, 20);
@@ -16,6 +20,9 @@ class Game {
         this._stopButton.addEventListener("click", this.stopGame.bind(this));
         this._gameLoop = null;
         this._renderEngine = new RenderEngine(this._gameView.context);
+        this._snake = new Snake(3, "#FFA500", "#00FF00", this._gameView.gridSquareSize, this._gameView.gridSquareSize, this._gameView.gridSquareSize * 2, this._gameView.gridSquareSize * 2);
+        this._apple = new Apple(this._gameView.gridSquareSize * 5, this._gameView.gridSquareSize * 5, this._gameView.gridSquareSize, this._gameView.gridSquareSize, "#FF0000");
+        this._p1 = new Controller('w', 's', 'a', 'd', this._snake, this._gameView.gridSquareSize);
     }
 
     startGame = () => {
@@ -28,10 +35,10 @@ class Game {
     }
     gameLoop = () => {
         this._gameView.context.clearRect(0, 0, this._gameView.canvas.width, this._gameView.canvas.height);
-        let snake = new Snake(3, "#FFA500", "#00FF00", this._gameView.gridSquareSize, this._gameView.gridSquareSize, this._gameView.gridSquareSize * 2, this._gameView.gridSquareSize * 2);
-        let apple = new Apple(this._gameView.gridSquareSize * 5, this._gameView.gridSquareSize * 5, this._gameView.gridSquareSize, this._gameView.gridSquareSize, "#FF0000");
-        this._renderEngine.addRenderable(snake);
-        this._renderEngine.addRenderable(apple);
+
+        this._p1.update();
+        this._renderEngine.addRenderable(this._snake);
+        this._renderEngine.addRenderable(this._apple);
         this._renderEngine.render();
         console.log("game loop");
     }
