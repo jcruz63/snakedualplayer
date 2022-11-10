248db89aafb740ad2a0949684c69b4f7da8d5dd4
jcruz63 <46271776+jcruz63@users.noreply.github.com>
2022-11-10 14:07:43 -0500

    completed snake body

--- src/Snake.ts
+++ src/Snake.ts
@@ -1,12 +1,13 @@
-class segment {
+class Segment {
     private _x: number;
     private _y: number;
-    //todo: add color
-    constructor(x: number, y: number) {
+    private _color: string;
+
+    constructor(x: number, y: number, color: string) {
         this._x = x;
         this._y = y;
+        this._color = color;
     }
-
 
     get x(): number {
         return this._x;
@@ -23,6 +24,15 @@ class segment {
     set y(value: number) {
         this._y = value;
     }
+
+
+    get color(): string {
+        return this._color;
+    }
+
+    set color(value: string) {
+        this._color = value;
+    }
 }
 
 class Snake implements Renderable{
@@ -31,23 +41,30 @@ class Snake implements Renderable{
     private _bodyColor: string = '#00FF00';
     private _width: number;
     private _height: number;
-    private _x: number = 0;
-    private _y: number = 0;
-    private _segments: segment[] = [];
-    //todo: ref to head segment
-    //todo: ref to tail segment
+    private _segments: Segment[] = [];
+    private _headSegment: Segment | undefined;
+    private _tailSegment: Segment | undefined;
 
 
-    //todo: remove x, y from the snake because it is the head
     constructor(length: number, headColor: string, bodyColor: string, width: number, height: number, x: number, y: number) {
-        this._length = length;
+        if(length > 3){
+         this._length = length;
+        }
         this._headColor = headColor;
         this._bodyColor = bodyColor;
         this._width = width;
         this._height = height;
-        this._x = x;
-        this._y = y;
-        //todo: add segments
+        for (let i = 0; i < length; i++) {
+            if(i === 0){
+                this._headSegment = new Segment(x, y, headColor);
+                this._segments.push(this._headSegment);
+            }else if(i === length - 1){
+                this._tailSegment = new Segment(x, y + (i * height), bodyColor);
+                this._segments.push(this._tailSegment);
+            } else {
+                this._segments.push(new Segment(x, y + (i * height), bodyColor));
+            }
+        }
     }
 
     get length(): number {
@@ -90,34 +107,68 @@ class Snake implements Renderable{
         this._height = value;
     }
 
-    //todo: get x and y should be the head segment x and y
-    //todo: set x and y should be the head segment x and y
+    //todo: get x and y should be the head Segment x and y
+    //todo: set x and y should be the head Segment x and y
     get x(): number {
-        return this._x;
+        // @ts-ignore
+        return this._headSegment.x;
     }
 
     set x(value: number) {
-        this._x = value;
+
+        let previousX;
+        let newX = value;
+        let previousY;
+        // @ts-ignore
+        let newY = this._headSegment.y;
+        this._segments.forEach(segment => {
+            previousX = segment.x;
+            segment.x = newX;
+            newX = previousX;
+            previousY = segment.y;
+            segment.y = newY;
+            newY = previousY;
+        });
+
     }
 
     get y(): number {
-
-        return this._y;
+        // @ts-ignore
+        return this._headSegment.y;
     }
 
     set y(value: number) {
-        this._y = value;
+        let previousY;
+        let newY = value;
+        let previousX;
+        // @ts-ignore
+        let newX = this._headSegment.x;
+        this._segments.forEach(segment => {
+            previousY = segment.y;
+            segment.y = newY;
+            newY = previousY;
+            previousX = segment.x;
+            segment.x = newX;
+            newX = previousX;
+        })
     }
 
-    //todo: addSegment. use the tail segment x and y to add a new segment and tail segment ref to the new segment
+    addSegment(): void {
+        // @ts-ignore
+        let x = this._tailSegment.x;
+        // @ts-ignore
+        let y = this._tailSegment.y + this._height;
+        this._tailSegment = new Segment(x, y, this._bodyColor);
+        this._segments.push(this._tailSegment);
+    }
 
     render(context: CanvasRenderingContext2D): void {
-        // Setting head color
-        context.fillStyle = this.headColor;
-        context.fillRect(this._x, this._y, this._width, this._height)
-        // Setting body
-        context.fillStyle = this.bodyColor;
-        //todo: render segments update x,y
+
+        this._segments.forEach((segment, index) => {
+            context.fillStyle = segment.color;
+            context.fillRect(segment.x, segment.y, this._width, this._height);
+            console.log(`Segment ${index} x: ${segment.x} y: ${segment.y}`);
+        });
 
 
     }
